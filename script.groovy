920f6fc8c434913a41b2c5cb14d687afd4960792
obaydah bouifadene <74828398+oubaydos@users.noreply.github.com>
2022-03-20 18:28:39 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,16 @@
-def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
-} 
-
-def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+def buildJar(){
+    echo "building the jar"
+    sh 'mvn package' 
+}
+def buildImage(){
+    echo "building the docker image"
+    sh 'docker build -t oubaydos/temp:jenkins_file .' 
+}
+def pushImage(){
+    echo "pushing the docker image"
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]){
+        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+        sh 'docker push oubaydos/temp:jenkins_file'
     }
-} 
-
-def deployApp() {
-    echo 'deploying the application...'
-} 
-
+}
 return this
